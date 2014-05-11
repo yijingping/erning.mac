1a4ac43f0ea9f692b1f0843a08fc41f911ab7b13
yijingping <jingping.yi@gmail.com>
2014-05-11 17:27:52 +0800

    config my bash/git/tmux/vim, inspired from erning

--- bin/ghmarkdown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HOME="/Users/erning"
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-source $HOME/.pythonbrew/etc/bashrc
-
-exec "$HOME/bin/github-flavored-markdown.rb" $@
-
--- sublime/User/Marked.py
+++
@@ -1,6 +0,0 @@
-import sublime, sublime_plugin
-
-class MarkedCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		filename = self.view.file_name();
-		self.view.window().run_command("exec", {"cmd": ["open", "-a", "Marked.app", filename]})
